0d8838975226a4904b5267fed9952194e8f59773
zval2 <adolfo.valerio21@gmail.com>
2026-08-03 22:52:35 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const button=document.getElementById("openBtn");
+
+const music=document.getElementById("music");
+
+button.onclick=()=>{
+
+music.play();
+
+alert("¡Bienvenidos! La siguiente versión abrirá la invitación con una animación de lujo.");
+
+}
